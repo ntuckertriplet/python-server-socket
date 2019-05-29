1eaab24fbdcf516934b4c9172408c94d71c314ca
Nate <ntuckertriplet@gmail.com>
2019-05-29 14:59:48 +0000

    simple tcp server

---
+++ tcp_server.py
@@ -0,0 +1,21 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostbyname()
+port = 4444
+
+server_socket.bind((host, port))
+
+server_socket.listen(20)
+
+while True:
+    client_socket, address = server_socket.accept()
+
+    print("received connection from " % str(address))
+
+    message = "thank you for connecting " + "\r\n"
+    client_socket.send(message)
+    
+    client_socket.close()
+
