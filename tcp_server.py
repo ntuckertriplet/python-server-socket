e157c45ef8d9a2cd3459fba931f0e80b7adb1dd8
Nate <ntuckertriplet@gmail.com>
2019-05-29 15:14:30 +0000

    simple tcp client

--- tcp_server.py
+++ tcp_server.py
@@ -12,10 +12,10 @@ server_socket.listen(20)
 while True:
     client_socket, address = server_socket.accept()
 
-    print("received connection from " % str(address))
+    print("received connection from %s" % str(address))
 
     message = "thank you for connecting " + "\r\n"
-    client_socket.send(message)
-    
+    client_socket.send(message.encode('ascii'))
+
     client_socket.close()
 
